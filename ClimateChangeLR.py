89877e00ad5199eda34f465b5ded7562087e9793
Fernando Coz <62630211+ferdcoz@users.noreply.github.com>
2020-10-04 21:27:39 -0300

    Add files via upload

---
+++ ClimateChangeLR.py
@@ -0,0 +1,56 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import seaborn as sns; sns.set()
+
+yearsBase, meanBase = np.loadtxt('https://raw.githubusercontent.com/ferdcoz/ClimateChangeLR/master/5-year-mean-1951-1980.csv', delimiter=',', usecols=(0, 1), unpack=True)
+years, mean = np.loadtxt('https://raw.githubusercontent.com/ferdcoz/ClimateChangeLR/master/5-year-mean-1882-2014.csv', delimiter=',', usecols=(0, 1), unpack=True)
+
+plt.scatter(yearsBase, meanBase)
+plt.title('scatter plot of mean temp difference vs year')
+plt.xlabel('years', fontsize=12)
+plt.ylabel('mean temp difference', fontsize=12)
+plt.show()
+
+# Creates a linear regression from the data points
+m,b = np.polyfit(yearsBase, meanBase, 1)
+
+# This is a simple y = mx + b line function
+def f(x):
+    return m*x + b
+
+# This generates the same scatter plot as before, but adds a line plot using the function above
+plt.scatter(yearsBase, meanBase)
+plt.plot(yearsBase, f(yearsBase))
+plt.title('scatter plot of mean temp difference vs year')
+plt.xlabel('years', fontsize=12)
+plt.ylabel('mean temp difference', fontsize=12)
+plt.show()
+
+# Prints text to the screen showing the computed values of m and b
+print(' y = {0} * x + {1}'.format(m, b))
+plt.show()
+
+# Pick the Linear Regression model and instantiate it
+model = LinearRegression(fit_intercept=True)
+
+# Fit/build the model
+model.fit(yearsBase[:, np.newaxis], meanBase)
+mean_predicted = model.predict(yearsBase[:, np.newaxis])
+
+# Generate a plot like the one in the previous exercise
+plt.scatter(yearsBase, meanBase)
+plt.plot(yearsBase, mean_predicted)
+plt.title('scatter plot of mean temp difference vs year')
+plt.xlabel('years', fontsize=12)
+plt.ylabel('mean temp difference', fontsize=12)
+plt.show()
+
+print(' y = {0} * x + {1}'.format(model.coef_[0], model.intercept_))
+
+plt.scatter(years, mean)
+plt.title('scatter plot of mean temp difference vs year')
+plt.xlabel('years', fontsize=12)
+plt.ylabel('mean temp difference', fontsize=12)
+sns.regplot(yearsBase, meanBase)
+plt.show()
